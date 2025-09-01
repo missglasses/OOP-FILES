0b98ef53b6126c70441d934bf7d91b63bb530bf4
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-09-01 11:14:00 +0800

    Create List_generic-collecs.cs

---
+++ c-sharp/List_generic-collecs.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic; //classes are defined generally without a specific type in mind
+
+namespace LearnLists
+{
+  class Program
+  {
+    static void Main()
+    {
+      List<bool> b = new List<bool>();
+      List<Random> r = new List<Random>();
+      List<IServiceProvider> i = new List<IServiceProvider>();
+    }
+  }
+}
