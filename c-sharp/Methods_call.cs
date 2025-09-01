397dc9eac96db583ad56135005b48d256a91099c
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-09-01 11:12:46 +0800

    Create Methods_call.cs

---
+++ c-sharp/Methods_call.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace CallingMethods
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string designer = "Anders Hejlsberg";
+      // Console.WriteLine is a method
+      Console.WriteLine(designer);
+
+      
+    }
+  }
+}
