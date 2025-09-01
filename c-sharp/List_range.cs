04618c077d8b2185505152177d3871178119375f
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-09-01 11:13:18 +0800

    Create List_range.cs

---
+++ c-sharp/List_range.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+namespace LearnLists
+{
+  class Program
+  {
+    static void Main()
+    {
+      List<double> marathons = new List<double>
+      {
+        144.07,
+        143.12,
+        146.73,
+        146.33
+      };
+      
+      //get range -first 3 elements
+      List<double> topMarathons = marathons.GetRange(0,3);
+      
+      //store result @new list: topmarathons
+      foreach (double d in topMarathons)
+      {
+        Console.WriteLine(d);
+      }
+    }
+  }
+}
