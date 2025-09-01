e288e4a2b1b7bc7d9c65f202808c2d6d809b8b2a
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-09-01 11:14:58 +0800

    Create Classes_definition.cs

---
+++ c-sharp/Classes_definition.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DefiningClasses
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Forest f = new Forest(); //empty class with instance F
+    }
+  }
+}
