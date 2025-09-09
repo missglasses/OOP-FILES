9ceb2e7d12ca7aeb848d4028a0e5e5eb86663575
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-09-09 14:47:01 +0800

    Create Performance.cs

---
+++ c-sharp/School-Performance/Performance.cs
@@ -0,0 +1,26 @@
+using System;
+
+    
+    public class Performer {
+        
+                
+                private string name;
+                private int age;
+                
+                public Performer(string name, int age){
+                    this.name=name;
+                    this.age=age;
+                }
+                
+                public string GetName(){
+                    return name;
+                }
+                
+                
+                public int GetAge(){
+                    return age;
+                }
+                
+        }
+        
+ 
